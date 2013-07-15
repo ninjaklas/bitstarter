9298a1604296f6ddca1b88ece22563fabd0322e6
ninjaklas <vidar@vasko.se>
2013-07-15 16:50:04 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,20 +1,10 @@
 var express = require('express');
-var fs = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-
-    var contents = fs.readFile('index.html','utf8',function(err,data) {
-	if(err) {
-	    response.send('unable to load the requested file');
-	}
-	else {
-	    response.writeHead(200, {'Content-Type': 'text/html'});
-	    response.write(data);
-	}
-	response.end();
-    });
+    response.send('Hello world 22');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
